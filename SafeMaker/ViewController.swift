9dae71193484ea7ba59ae8cc15e4ad9c5cd2bea3
noirdemort <darkStillwater@protonmail.com>
2020-06-21 12:56:43 +0530

    Initial Commit

---
+++ SafeMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafeMaker
+//
+//  Created by Noirdemort on 21/06/20.
+//  Copyright © 2020 Noirdemort. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
